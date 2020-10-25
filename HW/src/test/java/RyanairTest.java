2e70dc012716fb0fe8a906c58b6a85a8a931062e
Marina Zelenskaya <osychenkomarina@gmail.com>
2020-10-25 17:55:02 +0200

    'Selenide'

---
+++ HW/src/test/java/RyanairTest.java
@@ -0,0 +1,47 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class RyanairTest {
+
+
+    @Test
+    public void searchTicketsRyanair(){
+        Configuration.startMaximized = true;
+        Configuration.baseUrl = "https://www.ryanair.com/uk/uk";
+        open("/");
+        Configuration.timeout = 10000;
+
+
+
+        $(By.id("input-button__departure")).sendKeys("Vienna");
+        $(".list__clear-selection.ry-button--anchor-blue").click();
+        $(By.id("input-button__departure")).sendKeys("Vienna");
+        $x("//span[contains(text(),' Austria ')]").click();
+        $(By.xpath("//span[contains(text(),' Vienna ')]")).click();
+        $(By.id("input-button__destination")).sendKeys("Kyiv");
+        $(By.xpath("//span[contains(text(),' Ukraine ')]")).click();
+        $(By.xpath("//span[contains(text(),' Kyiv ')]")).click();
+
+        $(By.xpath("//div[@data-id='2020-11-19']")).click();
+        $(By.xpath("//div[@data-id='2020-11-21']")).click();
+
+        $$x("//div[@class='counter__button-wrapper--enabled']").first().click();
+        $(".passengers__confirm-button.ry-button--anchor-blue").click();
+
+        $(By.xpath("//button[@data-ref='flight-search-widget__cta']")).click();
+
+        $$(".card-header.b2").shouldHaveSize(2);
+        $(By.xpath("//button[@data-ref='2020-11-19']//div[@class='date-item__info date-item__info--selected']")).shouldBe(Condition.visible);
+        $(By.xpath("//button[@data-ref='2020-11-21']//div[@class='date-item__info date-item__info--selected']")).shouldBe(Condition.visible);
+        $$x("//h3[@class='header__title']").get(0).shouldHave(Condition.text(" Vienna to Kyiv  "));
+        $$x("//h3[@class='header__title']").get(1).shouldHave(Condition.text(" Kyiv to Vienna  "));
+
+
+
+
+    }
+}
---
+++ HW/src/test/java/UIA.java
@@ -0,0 +1,69 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.security.Key;
+
+import static com.codeborne.selenide.Selenide.*;
+import static java.awt.Event.ENTER;
+
+public class UIA {
+
+    @Test
+    public void UkraineInternationalAirlinesTest() throws AWTException {
+        Configuration.startMaximized = true;
+        Configuration.baseUrl = "https://www.flyuia.com";
+        open("/");
+        Configuration.timeout = 10000;
+
+        $$(".ng-untouched.ng-pristine.ng-valid").first().shouldBe(Condition.visible);
+        sleep(3000);
+        $$(".ng-untouched.ng-pristine.ng-valid").first().click();
+        $$(".ng-untouched.ng-pristine.ng-valid").first().click();
+
+
+        Robot robot = new Robot();
+
+        robot.keyPress(KeyEvent.VK_V);
+        robot.keyPress(KeyEvent.VK_I);
+        robot.keyPress(KeyEvent.VK_E);
+        robot.keyPress(KeyEvent.VK_N);
+        sleep(3000);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        $$(".ng-untouched.ng-pristine.ng-valid").get(0).click();
+        Robot robotNext = new Robot();
+        robotNext.keyPress(KeyEvent.VK_K);
+        robotNext.keyPress(KeyEvent.VK_Y);
+        robotNext.keyPress(KeyEvent.VK_I);
+        robotNext.keyPress(KeyEvent.VK_V);
+
+        sleep(3000);
+        robotNext.keyPress(KeyEvent.VK_ENTER);
+        $x("//span[@class='obe-sw-icon-calendar-departure']").click();
+        $$x("//span[@class='prev-next-btn']").last().click();
+        $x("//button[contains(text(), '19')]").click();
+        $x("//span[@class='obe-sw-icon-calendar-arrivals']").click();
+        $x("//button[contains(text(), '22')]").click();
+        $x("//span[@class='obe-sw-icon-passenger']").click();
+        $$x("//button[contains(text(), '+ ')]").get(0).click();
+        $x("//span[@class='obe-sw-icon-promo-code']").click();
+        $x("//form[@class='search-flights-button']").click();
+
+
+
+        switchTo().window("Результаты поиска");
+        //close popup with promocode
+        $x("//i[@class='icon-close']").click();
+        $$x("//div[@class='product__title']").shouldHaveSize(2);
+        $x("//button[@class='date-container nonclickable ng-star-inserted']/div[contains(text(), '19')]").shouldBe(Condition.visible);
+        $x("//button[@class='date-container nonclickable ng-star-inserted']/div[contains(text(), '22')]").shouldBe(Condition.visible);
+        $x("//div[@class='product__title'][contains(text(), 'Варианты вылета: VIENNA - Kyiv')]").shouldBe(Condition.visible);
+        $x("//div[@class='product__title'][contains(text(), 'Kyiv - VIENNA')]").shouldBe(Condition.visible);
+
+
+    }
+}
